ebab14b7f0d7e45e5a2818748a3c2bc5a6eb0704
toymechqm <toyjtoy@gmail.com>
2016-11-30 18:15:20 -0500

    start atoi

---
+++ day03/ex08/ft_atoi.c
@@ -0,0 +1,22 @@
+// Reproduce the behavior of the function atoi (man atoi).
+
+#include <stdio.h>
+
+int ft_atoi(char *str)
+{
+    int negative;
+    int value;
+
+    negative = 0;
+
+    return (value);
+}
+
+
+int main()
+{
+    char *str;
+
+    str = "Hello World";
+    ft_atoi(str);
+}
