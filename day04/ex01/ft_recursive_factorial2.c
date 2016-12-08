6f0e53edfef4f51d53e276f54ffece2373493028
toymechqm <toyjtoy@gmail.com>
2016-12-08 16:47:36 -0500

    redo recursive factorial

---
+++ day04/ex01/ft_recursive_factorial2.c
@@ -0,0 +1,37 @@
+// Create a recursive function that returns the factorial of the number given as a parameter.
+// If there’s an error, the function should return 0.
+// 0   1
+// 1   1
+// 2   2
+// 3   6
+// 4   24
+// 5   120
+// 6   720
+// 7   5040
+// 8   40320
+// 9   362880
+// 10  3628800
+// 11  39916800
+// 12  479001600
+// 13  6227020800
+
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+    if (nb == 0 || nb == 1)
+    {
+        return (1);
+    }
+    else
+    {
+        return (nb * ft_recursive_factorial(nb -1));
+    }
+}
+
+int main()
+{
+    int nb;
+    nb = 4;
+    printf("%d", ft_recursive_factorial(nb));
+}
