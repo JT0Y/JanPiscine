5fa672c8008cc5f26041c4f1493f178f7badf6a3
toymechqm <toyjtoy@gmail.com>
2016-12-13 17:35:57 -0500

    finish putnumber

--- day05/ex01/ft_putnbr.c
+++ day05/ex01/ft_putnbr.c
@@ -10,19 +10,48 @@
 
 void ft_putnbr(int nb)
 {
-    if ((nb - '0') <= '9')
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        nb *= -1;
+    }
+    if ((nb+'0' <= '9') && (nb+'0' >= '0'))
     {
         nb = nb + '0';
         write(1, &nb, 1);
     }
     else
-        return;
+    {
+        ft_putnbr(nb/10);
+        ft_putnbr(nb%10);
+    }
 }
 
 int main()
 {
-    int nb;
+    int nb1;
+    int nb2;
+    int nb3;
+    int nb4;
+    int nb5;
+    int nb6;
 
-    nb = 2;
-    ft_putnbr(nb);
+    nb1  = 3;
+    nb2 = +4;
+    nb3 = 83;
+    nb4 = 193;
+    nb5 = 100;
+    nb6 = -32;
+
+    ft_putnbr(nb1);
+    write(1, "\n", 1);
+    ft_putnbr(nb2);
+    write(1, "\n", 1);
+    ft_putnbr(nb3);
+    write(1, "\n", 1);
+    ft_putnbr(nb4);
+    write(1, "\n", 1);
+    ft_putnbr(nb5);
+    write(1, "\n", 1);
+    ft_putnbr(nb6);
 }
